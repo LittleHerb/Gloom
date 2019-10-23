761692dc58ea15b4d902ace21690f12d6c3821da
LittleHerb <LittleHerb@users.noreply.github.com>
2019-10-23 20:07:53 +1100

    shootshootbang

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    void OnCollisionEnter(Collision c)
+    {
+
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+    public Transform FirePoint;
+    public float bulletSpeed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+
+    }
+
+    private void Fire()
+    {
+        GameObject Rocket = Instantiate(rocketPrefab);
+        //Physics.IgnoreCollision(Rocket.GetComponent<Collider>(), FirePoint.parent.GetComponent<Collider>());
+
+        Rocket.transform.position = FirePoint.position;
+
+        Vector3 rotation = Rocket.transform.rotation.eulerAngles;
+
+        Rocket.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+
+        Rocket.GetComponent<Rigidbody>().AddForce(FirePoint.forward * bulletSpeed, ForceMode.Impulse);
+
+    }
+
+}
+
+/*{
+    public GameObject rocketPrefab;
+    public Transform firePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            GameObject g = (GameObject)Instantiate(rocketPrefab, transform.firePoint, transform.parent.rotation);
+
+
+            float force = g.GetComponent<Rocket>().speed;
+            g.GetComponent<Rigidbody>().AddForce(g.transform.forward * force);
+        }
+    }
+}*/
